b430bfaf96060339559dc211a271f680d36938cd
MateoVG <mateovegi@gmail.com>
2019-11-05 20:44:20 -0500

    Add example to template

--- Examples/Problem_set_template.py
+++ Examples/Problem_set_template.py
@@ -6,7 +6,7 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.2'
-#       jupytext_version: 1.2.3
+#       jupytext_version: 1.2.4
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
@@ -16,20 +16,25 @@
 # %% [markdown]
 # # YOUR NAME
 #
-# This notebook is a template showing you how to use some tools that will be useful for problem set 5
+# This notebook is a template showing you how to use some tools that will be useful for problem set 5.
+#
+# The tools are:
+# 1. Qmod: an implementation of the q-Model as a class in python. This is used to obtain phase diagrams.
+# 2. Dolo: a general purpose tool used to represent and solve economic models. This is used to compute optimal responses.
+#
+# The use of both tools will make us have to handle two representations of our problems, one as a Qmod object, and one as a Dolo model.
+#
+# Start by loading the tools we will be using.
 
 # %% {"code_folding": []}
 # Preamble: import the packages we will be using
+
+# Usual packages
 import numpy as np
 import matplotlib.pyplot as plt
-
 from copy import deepcopy
-from scipy import optimize
-
 from dolo import *
 import dolo.algos.perfect_foresight as pf
-import dolo.algos.value_iteration as vi
-
 import pandas as pd
 
 # Import the Qmod python code from external file
@@ -40,21 +45,19 @@ import sys
 sys.path.append('../')
 from Qmod.Q_investment import Qmod
 
-# %% {"code_folding": [0]}
+# %% [markdown]
+# I then define a function that takes two Qmod objects and plots their phase diagrams in the same figure.
+
+# %% {"code_folding": []}
 def phase_diagrams(mod1,mod2,npoints = 300):
     """
-    Computes (optimal) capital and lambda dynamics in face of a structural
-    change in the Q investment model.
+    Draws the phase diagram of the Qmodel under two different sets of
+    parameter values in the same figure, and returns it.
 
     Parameters:
-        - mod1    : Qmod object representing the parameter values prevailing
-                    from time 0 to t_change-1.
-        - mod2    : Qmod object representing the parameter values prevailing
-                    from time t_change onwards.
+        - mod1    : Qmod object representing the first set of parameter values.
+        - mod1    : Qmod object representing the second set of parameter values.
         - k0      : initial value for capital.
-        - t_change: time period at which the structural change takes place. It
-                    is assumed that the change is announced at period 0.
-        - T_sim   : final time period of the simulation.
         - npoints : number of points in the capital grid to be used for phase
                     diagram plots.
     """
@@ -80,28 +83,8 @@ def phase_diagrams(mod1,mod2,npoints = 300):
         ax.plot(mods[i].kss,mods[i].P,marker = '*', color = colors[i])
 
     return(ax)
-
-def simul_change_dolo(model, k0,  exog0, exog1, t_change, T_sim):
-
-    # The first step is to create time series for the exogenous variables
-    exog = np.array([exog1,]*(T_sim - t_change))
-    if t_change > 0:
-        exog = np.concatenate((np.array([exog0,]*(t_change)),
-                               exog),
-                              axis = 0)
-    exog = pd.DataFrame(exog, columns = ['R','tau','itc_1','psi'])
-
-    # Simpulate the optimal response
-    dr = pf.deterministic_solve(model = model,shocks = exog, T=T_sim,
-                                verbose=True, s1 = k0)
-
-    # Dolo uses the first period to report the steady state
-    # so we ommit it.
-    return(dr[1:])
-
-
 # %% [markdown]
-# Now I create a base model parametrization using both the Qmod class and the Dolo implementation.
+# Now I create a base model parametrization.
 
 # %%
 # Base parameters
@@ -128,51 +111,65 @@ delta = 0.1
 # Technological factor
 psi = 1
 
+# %% [markdown]
+# And create the objects representing the model. First in Qmod:
 
+# %%
 ## Qmod python class
+
+# Create the object.
 Qmodel = Qmod(beta, tau, alpha, omega, zeta, delta, psi)
+# Solve to find the steady state and policy rule.
 Qmodel.solve()
 
+# %% [markdown]
+# Then in Dolo:
+
+# %%
 ## Dolo
+
+# First import the external file with the model description.
 QDolo = yaml_import("../Dolo/Q_model.yaml")
-# We do not pass psi, tau, or zeta since they are handled not as parameters
-# but exogenous variables.
-QDolo.set_calibration(R = R, alpha = alpha, delta = delta, omega = omega)
+
+# Then replace the default parameters with our desired ones.
+QDolo.set_calibration(alpha = alpha, delta = delta, omega = omega)
+
+#############
+# IMPORTANT #
+#############
+
+# We do not pass R, psi, tau, or itc because they are handled not as parameters
+# but exogenous variables that can change over time. (see below)
 
 # %% [markdown]
-# ## Examples: changes in productivity.
-# %% {"code_folding": [0]}
+# ## Dynamic response example: changes in productivity.
+#
+# I now give an example of how to use Dolo to solve for the optimal behavior of the firm.
+# The basic idea is:
+# 1. We will create a path for the exogenous variables of the model.
+# 2. Dolo assumes that this path is announced to the firm at the first time period, and it computes the optimal dynamics according to these paths.
+# %%
 # Set up the basic features of the simulation
 
 # Total simulation time
-T = 20
+T = 100
 # Initial level of capital
 k0 = Qmodel.kss
 
-# Level to be changed
-psi_high = 1.3
+# %% {"code_folding": []}
+# Design the shock:
 
-# 
-# Copy the initial model, set the higher psi and re-solve
-Q_high_psi = deepcopy(Qmodel)
-Q_high_psi.psi = psi_high
-Q_high_psi.solve()
-
-# Draw the two phase diagrams and save them in an object
-# to add the plots of dynamics later.
-ax = phase_diagrams(mod1 = Qmodel, mod2 = Q_high_psi)
-
-# Now use dolo to find the optimal response to the shock.
-
-# First design the shock:
 # for this example, I am assuming that productivity increases and then
 # goes back to the original level.
+
+psi_high = 1.2
 Psi_sequence = np.array([psi]*3+
-                        [psi_high]*5+
-                        [psi]*(T-3-5))
+                        [psi_high]*50+
+                        [psi]*(T-3-50))
 # Check the pattern of the shock
 plt.figure()
 plt.plot(Psi_sequence)
+plt.ylabel('$\psi$')
 
 # Dolo receives a DataFrame with the full future paths for ALL exogenous
 # variables. So we create one:
@@ -181,164 +178,59 @@ Exog = pd.DataFrame({'R':[R]*T,
                      'itc_1':[zeta]*T,
                      'psi':Psi_sequence})
 
-soldolo = simul_change_dolo(model = QDolo, k0 = np.array([k0]),
-                            exog0 = [R,tau,zeta,psi],
-                            exog1 = [R,tau,zeta,psi_new],
-                            t_change = t, T_sim = T)
+# Examine the first few entries.
+Exog.head()
+# Note all other variables are left constant.
 
-# Plot the path of capital under both solutions
-time = range(T)
-plt.figure()
-plt.plot(time, sol['k'], 'x', label = 'Qmod', alpha = 0.8)
-plt.plot(time, soldolo['k'], '+', label = 'Dolo', alpha = 0.8)
-plt.legend()
-plt.title('Capital dynamics')
-plt.ylabel('$k_t$ : capital')
-plt.xlabel('$t$ : time')
+# %%
+# Now use the "perfect foresight" dolo solver
+response = pf.deterministic_solve(model = QDolo, # Model we are using (in dolo)
+                                  shocks = Exog, # Paths for exog. variables 
+                                  T=T,           # Total simulation time
+                                  s1 = [k0],     # Initial state
+                                  verbose=True)
+
+# Response is a DataFrame with the paths of every variable over time.
+# It adds information we don't need on the first row. So we delete it
+response = response[1:]
+
+# Inspect the first few elements.
+response.head()
+
 # %% [markdown]
-# ## 2. An increase in productivity announced at t=0 but taking effect at t=5
-# %% {"code_folding": []}
-# Repeat the calculation now assuming the change happens at t=5
-t = 5
+# # IMPORTANT
+#
+# Because of the way the model is implemented, it does not keep track of $\lambda_t$ or $ITC_t$, but  $\lambda_{t+1}$ and $ITC_{t+1}$ instead. 
+#
+# Thus, lambda_1 in row 1 corresponds to $\lambda_2$. Same with the ITC.
 
-# Qmod class
-sol = structural_change(mod1 = Qmodel, mod2 = Q_high_psi,
-                        k0 = k0, t_change = t,T_sim=T)
-
-# Dolo
-soldolo = simul_change_dolo(model = QDolo, k0 = np.array([k0]),
-                            exog0 = [R,tau,zeta,psi],
-                            exog1 = [R,tau,zeta,psi_new],
-                            t_change = t, T_sim = T)
-
-# Plot the path of capital under both solutions
-time = range(T)
-plt.figure()
-plt.plot(time, sol['k'], 'x', label = 'Qmod', alpha = 0.8)
-plt.plot(time, soldolo['k'], '+', label = 'Dolo', alpha = 0.8)
-plt.legend()
-plt.title('Capital dynamics')
-plt.ylabel('$k_t$ : capital')
-plt.xlabel('$t$ : time')
 # %% [markdown]
-# ## 3. An unanticipated corporate tax-cut
-# %% {"code_folding": [0]}
-# Set the taxes of the 'high-tax' scenario
-tau_high = 0.4
-# Set time of the change
-t = 0
+# # Plots
+#
+# Now we can use Qmod to plot the phase diagrams and add the optimal dynamics that we just found.
 
-# Qmod class
+# %%
+# Draw the two phase diagrams and save them in an object
+# to add the plots of dynamics later.
 
-# Copy the initial model, set a higher psi and re-solve
-Q_high_tau = deepcopy(Qmodel)
-Q_high_tau.tau = tau_high
-Q_high_tau.solve()
+# Copy the initial model, set the higher psi and re-solve
+Q_high_psi = deepcopy(Qmodel)
+Q_high_psi.psi = psi_high
+Q_high_psi.solve()
 
-# Capital will start at it steady state in the
-# high-tax scenario
-k0 = Q_high_tau.kss
+# Now we draw the phase diagrams of our base model "Qmodel"
+# and the new one "Q_high_psi", and store the plot in
+# object "ax"
+ax = phase_diagrams(mod1 = Qmodel, mod2 = Q_high_psi)
 
-sol = structural_change(mod1 = Q_high_tau, mod2 = Qmodel,
-                        k0 = k0, t_change = t,T_sim=T)
-
-# Dolo
-soldolo = simul_change_dolo(model = QDolo, k0 = np.array([k0]),
-                            exog0 = [R,tau_high,zeta,psi],
-                            exog1 = [R,tau,zeta,psi],
-                            t_change = t, T_sim = T)
-
-# Plot the path of capital under both solutions
-time = range(T)
-plt.figure()
-plt.plot(time, sol['k'], 'x', label = 'Qmod', alpha = 0.8)
-plt.plot(time, soldolo['k'], '+', label = 'Dolo', alpha = 0.8)
+# Now we can add the behavior of lambda and k to the diagram.
+ax.plot(response.k, response.lambda_1, '.k',label = 'Opt. Response')
 plt.legend()
-plt.title('Capital dynamics')
-plt.ylabel('$k_t$ : capital')
-plt.xlabel('$t$ : time')
-# %% [markdown]
-# ## 4. A corporate tax cut announced at t=0 but taking effect at t=5
-# %% {"code_folding": [0]}
-# Modify the time of the change
-t = 5
+plt.xlabel('k')
+plt.ylabel('$\\lambda$')
 
-# Qmod class
-sol = structural_change(mod1 = Q_high_tau, mod2 = Qmodel,
-                        k0 = k0, t_change = t,T_sim=T)
-
-# Dolo
-soldolo = simul_change_dolo(model = QDolo, k0 = np.array([k0]),
-                            exog0 = [R,tau_high,zeta,psi],
-                            exog1 = [R,tau,zeta,psi],
-                            t_change = t, T_sim = T)
-
-# Plot the path of capital under both solutions
-time = range(T)
+# New figure for capital dynamics
 plt.figure()
-plt.plot(time, sol['k'], 'x', label = 'Qmod', alpha = 0.8)
-plt.plot(time, soldolo['k'], '+', label = 'Dolo', alpha = 0.8)
-plt.legend()
-plt.title('Capital dynamics')
-plt.ylabel('$k_t$ : capital')
-plt.xlabel('$t$ : time')
-# %% [markdown]
-# ## 5. An unanticipated ITC increase
-# %% {"code_folding": [0]}
-# Set time of the change
-t=0
-# Set investment tax credit in the high case
-itc_high = 0.2
-# Set initial value of capital
-k0 = Qmodel.kss
-
-# Qmod class
-
-# Copy the initial model, set a higher psi and re-solve
-Q_high_itc = deepcopy(Qmodel)
-Q_high_itc.zeta = itc_high
-Q_high_itc.solve()
-
-sol = structural_change(mod1 = Qmodel, mod2 = Q_high_itc,
-                        k0 = k0, t_change = t,T_sim=T)
-
-# Dolo
-soldolo = simul_change_dolo(model = QDolo, k0 = np.array([k0]),
-                            exog0 = [R,tau,zeta,psi],
-                            exog1 = [R,tau,itc_high,psi],
-                            t_change = t, T_sim = T)
-
-# Plot the path of capital under both solutions
-time = range(T)
-plt.figure()
-plt.plot(time, sol['k'], 'x', label = 'Qmod', alpha = 0.8)
-plt.plot(time, soldolo['k'], '+', label = 'Dolo', alpha = 0.8)
-plt.legend()
-plt.title('Capital dynamics')
-plt.ylabel('$k_t$ : capital')
-plt.xlabel('$t$ : time')
-# %% [markdown]
-# ## 6. An ITC increase announced at t=0 but taking effect at t=5
-# %% {"code_folding": [0]}
-# Modify time of the change
-t = 5
-
-# Qmod class
-sol = structural_change(mod1 = Qmodel, mod2 = Q_high_itc,
-                        k0 = k0, t_change = t,T_sim=T)
-
-# Dolo
-soldolo = simul_change_dolo(model = QDolo, k0 = np.array([k0]),
-                            exog0 = [R,tau,zeta,psi],
-                            exog1 = [R,tau,itc_high,psi],
-                            t_change = t+1, T_sim = T)
-
-# Plot the path of capital under both solutions
-time = range(T)
-plt.figure()
-plt.plot(time, sol['k'], 'x', label = 'Qmod', alpha = 0.8)
-plt.plot(time, soldolo['k'], '+', label = 'Dolo', alpha = 0.8)
-plt.legend()
-plt.title('Capital dynamics')
-plt.ylabel('$k_t$ : capital')
-plt.xlabel('$t$ : time')
+plt.plot(response.k,'.')
+plt.ylabel('$k$')
+plt.xlabel('Time')
